e96c8c783d79a4b2860e08529cd992a38c9eaf7d
Koen Braham <k.braham@student.utwente.nl>
2016-10-31 13:54:54 +0100

    Reduced code for light changes. Using a function the behaviour is equal for both outputs

--- mq.lua
+++ mq.lua
@@ -37,88 +37,46 @@ m:on("offline", function(con)
      end)
 end)
 
+function change_led (led, target, ft)
+  t = target
+  c = lc[led]
+
+  diff=target-c
+  step = diff / (ft/led_update_time)
+
+  tmr.alarm(led, led_update_time, tmr.ALARM_AUTO, function() 
+    if (step > 0 and lc[led] + step < target) or (step < 0 and lc[led] + step > target) then
+      lc[led] = lc[led] + step;
+    else 
+      lc[led] = target
+      tmr.unregister(led)
+    end
+    pwm.setduty(led+2, lc[led])
+        
+  end)
+end
 -- on publish message receive event
 m:on("message", function(conn, topic, data)
-  print(topic .. ":" )
+  print(topic) 
   if data ~= nil then
     print(data)
-    payload = cjson.decode(data)
+    print(node.heap())
 
-    print("Input:"..data) 
+    payload = cjson.decode(data)['payload']
  
-    if payload['led'] == 1 then
-      led1_target=tonumber(payload['l'])
-      print("Received LED1 Target Value: "..led1_target)
-
-      led1_fadetimer=tonumber(payload['ft'])
-
-      print("Received LED1 Target Value: "..led1_target)
-      print("Received LED1 Current Value: " .. led1_current)
-
-      print ("Received LED1 Fadetimer: " ..led1_fadetimer)
-      led1_diff=(led1_target)-(led1_current)
-     
-      -- Get steps / second (on 10Hz).
-      led1_step = led1_diff / (led1_fadetimer/led1_update_time);
-    
-      
-      print("Step value: "..led1_step);
-
-      tmr.alarm(1, led1_update_time, 1, function() 
-        
-        if led1_step > 0 and led1_current + led1_step < led1_target then
-           led1_current = led1_current + led1_step;
-        elseif led1_step < 0 and led1_current + led1_step > led1_target then
-          led1_current = led1_current + led1_step;
-        else 
-          led1_current = led1_target;
-          
-          tmr.stop(1)
-        end;
-        
-        pwm.setduty(3, led1_current)
-        
-      end )
+    if topic == topic1 then
+      t=tonumber(payload['l'])
+      ft=tonumber(payload['ft'])
+      change_led(1,t,ft)
+    elseif topic == topic2 then
+      t=tonumber(payload['l'])
+      ft=tonumber(payload['ft'])
+      change_led(2,t,ft)
     end
 
-    if payload['led'] == 2 then
-    
-      led2_target=tonumber(payload['l'])
-      print("Received LED2 Target Value: "..led2_target)
-
-      led2_fadetimer=tonumber(payload['ft'])
-
-      print("Received LED2 Target Value: "..led2_target)
-      print("Received LED2 Current Value: " .. led2_current)
-
-      print ("Received LED2 Fadetimer: " ..led2_fadetimer)
-      led2_diff=(led2_target)-(led2_current)
-     
-      -- Get steps / second (on 10Hz).
-      led2_step = led2_diff / (led2_fadetimer/led2_update_time);
-    
-      
-      print("Step value: "..led2_step);
-
-
-      tmr.alarm(2, led2_update_time, 1, function() 
-        
-        if led2_step > 0 and led2_current + led2_step < led2_target then
-           led2_current = led2_current + led2_step;
-        elseif led2_step < 0 and led2_current + led2_step > led2_target then
-          led2_current = led2_current + led2_step;
-        else 
-          led2_current = led2_target;
-          tmr.stop(2)
-        end;
-        
-        pwm.setduty(4, led2_current)
-        
-      end )
-    end
-	
   end
 end)
+
 
 tmr.alarm(0, 1000, 1, function()
  if wifi.sta.status() == 5 then
